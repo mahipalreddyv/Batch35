0ad6b280c4899ba49f0b9d38f08a9bdb2479df75
mahipalreddyv <mahipalreddy.hadoop@gmail.com>
2022-06-11 12:02:07 +0530

    uncommented code in streaming file

--- src/main/java/straming_readjson.scala
+++ src/main/java/straming_readjson.scala
@@ -8,10 +8,7 @@ import org.apache.spark.sql.functions._
 object straming_readjson {
     def main(args : Array[String]): Unit = {  
       
-/*    var schmapath= "D:\\jsonfiles\\batch35_schema.json"
-    var datapath="D:\\jsonfiles\\data\\"
-    var chkpointdir="D:\\jsonfiles\\checkdir"
-    var output="D:\\jsonfiles\\output\\"*/
+
       
     val spark=SparkSession.builder().master("local[*]").appName("Readjson").getOrCreate()
     spark.conf.set("spark.sql.straming.checkpointLocation","D:\\jsonfiles\\checkdir")
@@ -36,7 +33,7 @@ object straming_readjson {
   var dedup=selectExpr.withColumn("rank", row_number over windowspec)
   //println("####################",dedup.count())
   //selectExpr.writeStream.outputMode("Append").format("csv").option("path",output).start().awaitTermination()
-  //selcectExptDF.writeStream.outputMode("Append").format("csv").option("path", resultpath).start().awaitTermination()
+  selcectExptDF.writeStream.outputMode("Append").format("csv").option("path", resultpath).start().awaitTermination()
   selectExpr.writeStream.outputMode("Append").format("csv").option("path","D:\\jsonfiles\\output").start().awaitTermination()
  
   }
